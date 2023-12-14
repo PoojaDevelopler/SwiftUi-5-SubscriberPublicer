1c22cf0911d9f9e968605379f79e1b0eef9134bd
pooja negi <pooja.negi@bolt.earth>
2023-12-14 13:27:30 +0530

    Initial Commit

---
+++ SwiftUi-5-SubscriberPublicer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUi-5-SubscriberPublicer
+//
+//  Created by pnkbksh on 14/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUi-5-SubscriberPublicer/SwiftUi_5_SubscriberPublicerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_5_SubscriberPublicerApp.swift
+//  SwiftUi-5-SubscriberPublicer
+//
+//  Created by pnkbksh on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_5_SubscriberPublicerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
